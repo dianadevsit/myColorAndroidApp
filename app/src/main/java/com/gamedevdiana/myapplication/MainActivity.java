03ddf4a64d67727ea1e45642fdc51449153b55d5
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-02-08 15:24:10 -0500

    added more private classes and layout references

--- app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
+++ app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mcolorInput;
+    private TextView mColorOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColorInput = findViewById(R.id.colorInput);
     }
 
     public void updateColorText(View view) {
