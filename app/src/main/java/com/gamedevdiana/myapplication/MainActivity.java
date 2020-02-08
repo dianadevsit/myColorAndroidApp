8d17b80f4c6913c6f1bac7539e017ed07cc4aafe
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-02-08 16:14:16 -0500

    added switch for user input

--- app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
+++ app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.gamedevdiana.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,8 +19,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mColorInput = findViewById(R.id.colorInput);
+        myColorOutput = findViewById(R.id.colorOutput);
     }
 
     public void updateColorText(View view) {
+        String color = mcolorInput.getText().toString().toLowerCase();
+        //Add a message for the String
+        String message = "";
+
+        switch (color) {
+            case blue:
+            message = "You like BLUE";
+            mColorOutput.setText(message);
+            mColorOutput.setTextColor(Color.BLUE);
+            break;
+            case green:
+                message = "You like GREEN";
+                mColorOutput.setText(message);
+                mColorOutput.setTextColor(Color.GREEN);
+                break;
+            case yellow:
+                message = "You like YELLOW";
+                mColorOutput.setText(message);
+                mColorOutput.setTextColor(Color.YELLOW);
+                break;
+            case black:
+                message = "You like BLACK";
+                mColorOutput.setText(message);
+                mColorOutput.setTextColor(Color.BLACK);
+                break;
+            default:
+                message = " You like COLORS";
+                mColorOutput.setText(message);
+                mColorOutput.setTextColor(Color.rbg(200,105, 210));
+                break;
+        }
     }
 }
