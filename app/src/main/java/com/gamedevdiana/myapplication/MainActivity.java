34ae4360d7fcc5aef888092f857eb1f77e948387
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-02-08 23:45:08 -0500

    finished product with emulator- PIXEL 2

--- app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
+++ app/src/main/java/com/gamedevdiana/myapplication/MainActivity.java
@@ -8,9 +8,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mcolorInput;
+    private EditText mColorInput;
     private TextView mColorOutput;
 
     @Override
@@ -19,39 +20,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mColorInput = findViewById(R.id.colorInput);
-        myColorOutput = findViewById(R.id.colorOutput);
+        mColorOutput = findViewById(R.id.colorOutput);
+
     }
 
     public void updateColorText(View view) {
-        String color = mcolorInput.getText().toString().toLowerCase();
+        String color = mColorInput.getText().toString().toLowerCase();
         //Add a message for the String
         String message = "";
 
         switch (color) {
-            case blue:
-            message = "You like BLUE";
-            mColorOutput.setText(message);
-            mColorOutput.setTextColor(Color.BLUE);
-            break;
-            case green:
+            case "blue":
+                message = "You like BLUE";
+                mColorOutput.setText(message);
+                mColorOutput.setTextColor(Color.BLUE);
+                break;
+            case "green":
                 message = "You like GREEN";
                 mColorOutput.setText(message);
                 mColorOutput.setTextColor(Color.GREEN);
                 break;
-            case yellow:
+            case "yellow":
                 message = "You like YELLOW";
                 mColorOutput.setText(message);
                 mColorOutput.setTextColor(Color.YELLOW);
                 break;
-            case black:
+            case "black":
                 message = "You like BLACK";
                 mColorOutput.setText(message);
                 mColorOutput.setTextColor(Color.BLACK);
                 break;
             default:
-                message = " You like COLORS";
+                message = "You like COLORS";
                 mColorOutput.setText(message);
-                mColorOutput.setTextColor(Color.rbg(200,105, 210));
+                mColorOutput.setTextColor(Color.rgb(200, 150, 210));
                 break;
         }
     }
